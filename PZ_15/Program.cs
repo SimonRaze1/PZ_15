a579269c8bd4a03d77d4eeaf2a7ad2d77f10a247
User <User@DESKTOP-AMGP8UK>
2023-12-04 20:38:56 +0500

    Добавьте файлы проекта.

---
+++ PZ_15/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace PZ_15
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = @"D:\фоточки\dd";
+            RenameFiles(path);
+        }
+        static void RenameFiles(string path)
+        {
+            string[] files = Directory.GetFiles(path, "*.txt");
+            foreach (string filepath in files)
+            {
+                string filename = Path.GetFileName(filepath);
+                string newname = filename + "NEW.txt";
+                string newpath = Path.Combine(path, newname);
+                File.Move(filepath, newpath);
+            }
+        }
+    }
+}
